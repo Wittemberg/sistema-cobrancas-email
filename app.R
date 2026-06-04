21b4aa568d95368e4cf0cc4af9a92881467c85b2
Wittemberg <wittemberg@hotmail.com>
2026-06-04 11:54:55 -0300

    Trace Shiny startup sources

--- app.R
+++ app.R
@@ -1,15 +1,29 @@
+log_startup <- function(...) {
+  message(...)
+  flush.console()
+}
+
+source_startup <- function(arquivo) {
+  log_startup("SOURCE_START=", arquivo)
+  source(arquivo)
+  log_startup("SOURCE_OK=", arquivo)
+}
+
+log_startup("APP_BOOT_VERSION=", Sys.getenv("APP_VERSION", unset = "dev"))
+log_startup("APP_BOOT_CWD=", getwd())
+
 library(shiny)
 
-source("R/01-config.R")
-source("R/02-dados.R")
-source("R/03-email.R")
-source("R/07-chatwoot.R")
-source("R/06-auth.R")
-source("R/04-ui.R")
-source("R/05-server.R")
+source_startup("R/01-config.R")
+source_startup("R/02-dados.R")
+source_startup("R/03-email.R")
+source_startup("R/07-chatwoot.R")
+source_startup("R/06-auth.R")
+source_startup("R/04-ui.R")
+source_startup("R/05-server.R")
 
-message("APP_VERSION=", Sys.getenv("APP_VERSION", unset = "dev"))
-message("EMAIL_WORKER=", file.exists(file.path(pasta_raiz, "R", "08-email-worker.R")))
-message("PASTA_RAIZ=", pasta_raiz)
+log_startup("EMAIL_WORKER=", file.exists(file.path(pasta_raiz, "R", "08-email-worker.R")))
+log_startup("PASTA_RAIZ=", pasta_raiz)
+log_startup("SHINY_APP_READY=TRUE")
 
 shinyApp(ui = ui, server = server)
